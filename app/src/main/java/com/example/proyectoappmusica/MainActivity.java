87a3c8bbb3e82e64c8c4beadb651745a6f81bd1c
Oscar Balbuena Tome <oscarbtome97@gmail.com>
2020-10-12 12:57:08 +0200

    Pantalla login en proceso.

--- app/src/main/java/com/example/proyectoappmusica/MainActivity.java
+++ app/src/main/java/com/example/proyectoappmusica/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.proyectoappmusica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etNombreUsuario;
+    private EditText etPassword;
+    private CheckBox cbRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNombreUsuario = (EditText)findViewById(R.id.etNombreUsuario);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        cbRegistrar = (CheckBox)findViewById(R.id.cbRegistrar);
+
     }
 }
