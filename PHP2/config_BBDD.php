85ed4bfe777a496ff063fc94c912c88b5745da9e
Oscar Balbuena Tome <oscarbtome97@gmail.com>
2020-10-14 19:45:13 +0200

    Subida de PHP

---
+++ PHP2/config_BBDD.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *Constantes para conectarse a la base de datos
+ */
+define("HOSTNAME", "localhost");
+define("DATABASE", "appmusica"); 
+define("USERNAME", "usuarioApp"); 
+define("PASSWORD", ""); 
+?>
